7364ce8919baf804fbd349ed11294b494d034afe
tdj4909 <tdj4909@gmail.com>
2025-03-10 14:28:43 +0900

    update

--- ezenFirst/src/main/java/com/a2a2lab/module/codeGroup/CodeGroupController.java
+++ ezenFirst/src/main/java/com/a2a2lab/module/codeGroup/CodeGroupController.java
@@ -1,5 +1,13 @@
 package com.a2a2lab.module.codeGroup;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class CodeGroupController {
 
+	@RequestMapping(value = "/codeGroup")
+	public String codeGroup() {
+		return "/xdm/codeGroup/codeGroupXdmList";
+	}
 }
