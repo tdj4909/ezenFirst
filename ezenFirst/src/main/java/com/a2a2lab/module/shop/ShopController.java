b3b467efae662428da7cfc8df19da6a36aeeaec4
tdj4909 <tdj4909@gmail.com>
2025-05-01 13:22:47 +0900

    페이징 Ajax

--- ezenFirst/src/main/java/com/a2a2lab/module/shop/ShopController.java
+++ ezenFirst/src/main/java/com/a2a2lab/module/shop/ShopController.java
@@ -3,7 +3,8 @@ package com.a2a2lab.module.shop;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -11,6 +12,7 @@ import com.a2a2lab.module.member.MemberService;
 import com.a2a2lab.module.product.ProductService;
 import com.a2a2lab.module.product.ProductVo;
 import com.a2a2lab.module.review.ReviewService;
+
 import jakarta.servlet.http.HttpSession;
 
 
@@ -26,7 +28,7 @@ public class ShopController {
 	
 	// 상품 리스트 화면
 	@RequestMapping(value = "/TableOrder/shopList")
-	public String shopList(Model model, HttpSession httpSession, @ModelAttribute("vo") ProductVo vo) {
+	public String shopList(Model model, HttpSession httpSession, ProductVo vo) {
 		
 		// login 검사
 		if(httpSession.getAttribute("user") == null) {
@@ -34,17 +36,25 @@ public class ShopController {
 		}
 		model.addAttribute("user", httpSession.getAttribute("user"));
 		
-		vo.setRowNumToShow(6);
+		return "/usr/shop/shopList";
+	}
+	// 메뉴 리스트 Ajax
+	@GetMapping("/TableOrder/menuList")
+	public String getMenuListFragment(@RequestParam(name = "page", defaultValue = "1") int page, Model model, ProductVo vo) {
+		
+		vo.setRowNumToShow(1);
+		vo.setThisPage(page);
 		vo.setParamsPaging(productService.selectOneCount(vo));
 		
+		model.addAttribute("vo", vo);
 		model.addAttribute("list", productService.selectShopList(vo));
 		
-		return "/usr/shop/shopList";
+		return "usr/shop/menuList :: menuListFragment";
 	}
 	
 	// 상품 상세 화면
-	@RequestMapping(value = "/TableOrder/shopDetail")
-	public String shopDetail(@RequestParam("formSeq") String seq, Model model, HttpSession httpSession) {
+	@RequestMapping(value = "/TableOrder/shopDetail/{seq}")
+	public String shopDetail(@PathVariable("seq") String seq, Model model, HttpSession httpSession) {
 		
 		// login 검사
 		if(httpSession.getAttribute("user") == null) {
@@ -55,7 +65,6 @@ public class ShopController {
 		ProductVo vo = new ProductVo();
 		vo.setIfcgSeq(seq);
 		model.addAttribute("item", productService.selectOne(vo));
-		
 		model.addAttribute("reviewList", reviewService.getReviewListByMenuSeq(seq));
 		
 		return "/usr/shop/shopDetail";
