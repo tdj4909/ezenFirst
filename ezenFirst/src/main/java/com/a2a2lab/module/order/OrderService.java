ffef024c66a74d589edde99cf7241ae725f52de3
tdj4909 <tdj4909@gmail.com>
2025-05-24 23:46:04 +0900

    order caching home

--- ezenFirst/src/main/java/com/a2a2lab/module/order/OrderService.java
+++ ezenFirst/src/main/java/com/a2a2lab/module/order/OrderService.java
@@ -1,11 +1,16 @@
 package com.a2a2lab.module.order;
 
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.a2a2lab.module.code.CodeDto;
+import com.a2a2lab.module.code.CodeService;
+import com.a2a2lab.module.product.ProductDto;
 import com.a2a2lab.module.vo.PageVo;
 import com.a2a2lab.module.vo.SearchVo;
 
@@ -14,6 +19,9 @@ public class OrderService {
 
 	@Autowired
 	OrderDao dao;
+	
+	@Autowired
+	CodeService codeService;
 	
 	public int countOrderMastersByVo(SearchVo searchVo) {
 		return dao.countOrderMastersByVo(searchVo);
@@ -32,7 +40,22 @@ public class OrderService {
 	}
 	
 	public OrderDto findOrderMasterById(String id) {
-		return dao.findOrderMasterById(id);
+		// 캐싱된 코드 맵에서 주문 상태 종류 코드그룹 가져오기
+	    List<CodeDto> codeDtos = codeService.getCodesByCodegroupName("주문 상태");
+
+	    // codeId → name 맵으로 변환
+	    Map<Integer, String> codeNameMap = new HashMap<>();
+	    for (CodeDto codeDto : codeDtos) {
+	        codeNameMap.put(Integer.parseInt(codeDto.getCodeId()), codeDto.getName());
+	    }
+
+	    // OrderMaster 가져오기
+	    OrderDto dto = dao.findOrderMasterById(id);
+	    
+	    // 주문 상태에 해당하는 이름 설정
+	    dto.setStatusName(codeNameMap.get(dto.getStatus()));
+
+	    return dto;
 	}
 	
 	public List<OrderDto> findOrderDetailsByOrderMasterId(String id) {
