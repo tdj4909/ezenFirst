a8ad8c372a607310f6f396b732ac6169020a0cdf
tdj4909 <tdj4909@gmail.com>
2025-05-26 12:16:17 +0900

    google login aca

--- ezenFirst/src/main/java/com/a2a2lab/common/config/CustomOAuth2UserService.java
+++ ezenFirst/src/main/java/com/a2a2lab/common/config/CustomOAuth2UserService.java
@@ -1,19 +1,28 @@
 package com.a2a2lab.common.config;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+
+import com.a2a2lab.module.member.MemberDao;
+import com.a2a2lab.module.member.MemberDto;
 
 @Service
 public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
 
+	@Autowired
+    MemberDao dao;
+	
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oauth2User = new DefaultOAuth2UserService().loadUser(userRequest);
@@ -21,12 +30,30 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
         // 예: 구글 로그인 시 사용자 이메일
         String email = oauth2User.getAttribute("email");
 
+        MemberDto member = dao.findMemberByEmail(email);
+
+        if (member == null){
+            throw  new UsernameNotFoundException("아이디 없음");
+        }
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+
+        return new CustomUserDetails(
+                member.getEmail(),
+                member.getPassword(),
+                member.getName(),
+                member.getMemberId(),
+                authorities,
+                oauth2User.getAttributes()
+        );
+        
         // 회원가입 되어 있으면 기존 사용자 리턴, 없으면 새로 등록
         // ex) DB에서 Member 찾아보고 없으면 insert
 
-        return new DefaultOAuth2User(
-                Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")),
-                oauth2User.getAttributes(),
-                "email"); // usernameKey (주로 email)
+//        return new DefaultOAuth2User(
+//                Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")),
+//                oauth2User.getAttributes(),
+//                "email"); // usernameKey (주로 email)
     }
 }
--- ezenFirst/src/main/java/com/a2a2lab/common/config/CustomUserDetails.java
+++ ezenFirst/src/main/java/com/a2a2lab/common/config/CustomUserDetails.java
@@ -1,17 +1,20 @@
 package com.a2a2lab.common.config;
 
 import java.util.Collection;
+import java.util.Map;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
-public class CustomUserDetails implements UserDetails {
+public class CustomUserDetails implements OAuth2User, UserDetails {
 
     private String email;
     private String password;
     private String name;
     private String memberId;
     private Collection<? extends GrantedAuthority> authorities;
+    private Map<String, Object> attributes;
 
     public CustomUserDetails(String email, String password, String name, String memberId,
                              Collection<? extends GrantedAuthority> authorities) {
@@ -21,6 +24,17 @@ public class CustomUserDetails implements UserDetails {
         this.memberId = memberId;
         this.authorities = authorities;
     }
+    
+    public CustomUserDetails(String email, String password, String name, String memberId,
+            Collection<? extends GrantedAuthority> authorities,
+            Map<String, Object> attributes) {
+		this.email = email;
+		this.password = password;
+		this.name = name;
+		this.memberId = memberId;
+		this.authorities = authorities;
+		this.attributes = attributes;
+	}
 
     public String getName() {
         return name;
@@ -28,6 +42,11 @@ public class CustomUserDetails implements UserDetails {
     
     public String getMemberId() {
         return memberId;
+    }
+    
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
     }
 
     @Override
@@ -68,7 +87,7 @@ public class CustomUserDetails implements UserDetails {
 	@Override
 	public String toString() {
 		return "CustomUserDetails [email=" + email + ", password=[PROTECTED], name=" + name + ", authorities="
-				+ authorities + "]";
+				+ authorities + ", attributes=" + attributes + "]";
 	}
     
     
